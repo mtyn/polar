d6d857fa7a66cb1e698041722ee0101234ec5cd8
Tim Hull <tim@thull.dev>
2023-03-11 13:21:52 +0000

    feat: fix the generator for jetbrains

--- generator/Sources/Generator/ports/jetbrains.swift
+++ generator/Sources/Generator/ports/jetbrains.swift
@@ -8,16 +8,15 @@ struct JetBrains: Port {
     var colorStyle: ColorStyle = .hex
     
     var relevantComponents: [PortComponent] = [
-        .file(path: "resources/themes/nord.xml", renameTo: "resources/Polar.xml"),
-        .file(path: "src/nord.theme.json", renameTo: "resources/theme/Polar.theme.json")
+        .file(path: "resources/themes/nord.xml", renameTo: "resources/theme/polar.xml"),
+        .file(path: "src/nord.theme.json", renameTo: "resources/theme/polar.theme.json")
     ]
     
     func extraReplacements(forTheme theme: PolarColorScheme) -> [Replacement] {
         [
             Replacement(find: "Arctic Ice Studio", replace: "Tim Hull", doInverseCapitalisation: false),
             Replacement(find: "\"dark\": true", replace: "\"dark\": false", doInverseCapitalisation: false),
-            Replacement(find: "Darcula", replace: "Light", doInverseCapitalisation: false),
-            Replacement(find: "/themes/Polar.xml", replace: "/Polar.xml", doInverseCapitalisation: false)
+            Replacement(find: "/themes/", replace: "/theme/", doInverseCapitalisation: false)
         ]
     }
 }
